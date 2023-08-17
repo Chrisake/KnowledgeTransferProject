d9ce1e3a249988fa15be19066cc9121fbbc87788
Christoforos Sakellaris <csakellaris@euronetworldwide.com>
2023-08-17 14:15:13 +0200

    fix_linux_CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "utils.h"
 #include <iostream>
+#include "logger.h"
 
 
 int main() {
+	std::cout << "version: " << __cplusplus << "\n";
 	std::cout << utils::reverse(1234) << "\n";
 	std::cout << utils::reverse(235617) << "\n";
 	std::cout << utils::reverse(5849648) << "\n";
 	std::cout << utils::reverse(19838419) << "\n";
 	std::cout << utils::reverse("Hello World") << "\n";
 	std::cout << utils::reverse("abcdEFGhijkLMNOP") << "\n";
-
 	return 0;
 }
